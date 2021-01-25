d47b7665b2a3d95be08b71f0812e2ecaa6c66931
Will <wlawrie93@gmail.com>
2021-01-25 14:44:18 +0100

    Added Function calculate area of circle and docstring

--- test.py
+++ test.py
@@ -1,2 +1,10 @@
-def myf():
-    print('myf')
+import numpy as np
+
+def circle_area(r):
+    '''
+    This function returns the area of a circle radius r
+    ========
+    Inputs:
+    r (float64): Radius of the circle
+    '''
+    return np.pi*r**2
