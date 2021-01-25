8aef73b51dfa8da9e442ddc218f51759cc7c69a3
Alberto <a.tosato@tudelft.nl>
2021-01-25 14:57:35 +0100

    albo commit

--- test.py
+++ test.py
@@ -3,6 +3,7 @@ import numpy as np
 def circle_area(r):
     '''
     This function returns the area of a circle radius r
+    The radius of my cricle of friends includes you <3
     ========
     Inputs:
     r (float64): Radius of the circle
